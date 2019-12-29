1b2b01990764bbd49701cbcb40b7d318935f8521
Curtis Downing <cdowning@mediatemple.net>
2019-12-28 17:26:14 -0800

    make matches less greedy returning only items that have values

--- exipick2json.py
+++ exipick2json.py
@@ -2,10 +2,10 @@
 
 # the only purpose of this script is to take exipick
 # output and convert the data into JSON
-# JSON should make it easy to import into any language
+# JSON should make it easy to import into any language or data stores
 # and perform some real logic instead of bash scripts
-# currently works with Python 2 - migration to 3 is needed
 
+# written as Python 2; migration to 3 is needed
 # example exipick command to run the output piped into this script
 
 # exipick -bpa --flatq --unsorted --show-vars message_exim_id,semder_host_address,sender_host_authenticated,sender_local,sender_set_untrusted,spam_score,warning_count,authenticated_id,allow_unqualified_sender,authenticated_sender,body_linecount,body_zerocount,deliver_frozen_at,deliver_freeze,dont_deliver,recipients,first_delivery,message_age,message_body_size,message_body,received_protocol,recipients_count,recipients_undel_count,sender_address,reply_address,sender_address_domain,sender_helo_name,message_linecount,message_size,sender_host_name,sender_ident,shown_message_size,smtp_active_hostname
@@ -14,11 +14,11 @@ import os
 import re
 import json
 import sys
-exipick_input = sys.stdin.read()
 
 class ExipickJson():
 
     def __init__(self):
+        self.exipick_input = sys.stdin.read()
         self.data = []
         self.output = []
 
@@ -29,7 +29,7 @@ class ExipickJson():
           'sender_address',
         ]
 
-        lines = exipick_input.splitlines()
+        lines = self.exipick_input.splitlines()
         for line in lines:
             self.output.append(line)
 
@@ -41,11 +41,11 @@ class ExipickJson():
         self.metadata['message_headers'] = []
         self.recipients = [] # reset recipents back to a blank slate (new id)
         self.metadata['recipients'] = self.recipients
-       
+
     # extract value from (key='value') strings as metadata
 
     def extract_metadata(self, message_key, message):
-        regex_string = '{}=\'(.*?)\''.format(message_key)
+        regex_string = '{}=\'(.+?)\''.format(message_key)
         m = re.search(regex_string, message)
         if m:
             self.metadata[message_key] = m.group(1)
@@ -61,7 +61,6 @@ class ExipickJson():
         self.metadata['message_headers'].append(h)
 
     def jsonipick(self):
-       
         # regular expressions for field extraction from exipick message
         # key is the metadata field name for the output JSON
 
@@ -118,7 +117,7 @@ o.set_show_vars([
    'sender_host_name',
    'sender_ident',
    'shown_message_size',
-   'smtp_active_hostname'
+   'smtp_active_hostname',
 ])
 
 # run extractions
